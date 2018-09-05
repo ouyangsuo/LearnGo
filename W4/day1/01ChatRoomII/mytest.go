bbeaee24365de3957adcadf8f8fad3482557ab17
sirouyang <284577461@qq.com>
2018-09-05 11:23:04 +0800

    删除不必要的代码

--- W4/day1/01ChatRoomII/mytest.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Println("2018-08-13 09:44:00 张全蛋#劳资有日志你有吗"[20:])
-}
--- W4/day1/03协程死锁/02单协程死锁.go
+++
@@ -1,9 +0,0 @@
-package main
-
-func main0() {
-	ch := make(chan int, 0)
-
-	//想法：自写自读——没人并发读取，永远写不进去，死锁
-	ch <- 1
-	<-ch
-}
